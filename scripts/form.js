1a0cc29dad1feb44acd05bfd395ab096a0bea975
Levi-F06 <levi.flanagan06@gmail.com>
2024-09-30 10:24:22 +0100

    :art: Change logic of form.js to decrease size
    This commit will change the query selector to use a class instead of
    the input element to avoid having to check for the submit button
    input.

--- scripts/form.js
+++ scripts/form.js
@@ -1,17 +1,12 @@
-// get form elements
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll(".mainInput");
 
 for (const input of inputs) {
-  if (input.type === "submit") {
-    continue;
-  }
   input.addEventListener("input", (e) => {
     for (const newInputValue of inputs) {
       if (e.target.value > 100) {
         e.target.value = 100;
       }
-      newInputValue.value =
-        newInputValue.type === "submit" ? "go draw!" : e.target.value;
+      newInputValue.value = e.target.value;
     }
   });
 }
